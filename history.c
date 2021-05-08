e6caacd3054879cb931007f94d4ea5f50234a2a6
Ethan Ng <wlx5502@outlook.com>
2021-05-08 15:21:07 +0800

    implemnet built-in: history

---
+++ history.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "history.h"
+
+int add_entry_to_history(const char *__line) {
+    /* into following branch at first call */
+    if (!historys) {
+        historys = init_historys();
+        if (!historys) {
+            fprintf(stderr, "error: memory allocation failed.\n");
+            return -1;
+        }
+    }
+
+    const char *line = strdup(__line);
+    if (!line) 
+        return -1;
+    
+    historys->cur = (historys->cur + 1) % MAX_HISTORY_ENTRIES;
+    historys->entries[historys->cur] = line;
+    if (historys->entry_count < MAX_HISTORY_ENTRIES) 
+        ++(historys->entry_count);
+
+    return 0;
+}
+
+int __history(int *argc, char *argv[]) {
+    int offset = historys->entry_count;
+    if (*argc > 1) {
+        /* case: history -c */
+        if (strncmp(argv[1], "-c", 2) == 0) {
+            /* strlen("-c") = 2 */
+            clear_entries_in_history();
+            return 0;
+        }   
+
+        /* case: history [n] */
+        char *end_ptr;
+        long offset_l;
+
+        /* 10 represents decimal (base = 10) */
+        offset_l = strtol(argv[1], &end_ptr, 10);
+        if (end_ptr == argv[1]) {
+            fprintf(stderr, "error: failed in convert to number.\n");
+            return -1;
+        }
+        offset = (offset_l > offset)? 
+                    offset : (int) offset_l;
+    }
+
+    /* output the wanted history entries */
+    int i = (historys->cur - offset + MAX_HISTORY_ENTRIES) % MAX_HISTORY_ENTRIES;
+    int entry_id = historys->entry_count - offset + 1;
+    do {
+        fprintf(stdout, "\t%d  %s\n", entry_id++, historys->entries[i]);
+    } while (i != historys->cur);
+
+    return 0;
+}
+
+void _free_all_in_history() {
+    clear_entries_in_history();
+    free(historys);
+    return ;
+}
--- history.h
+++ history.h
@@ -9,12 +9,33 @@
 struct history {
     int cur;
     int entry_count;
-    char **entries;
+    const char *entries[MAX_HISTORY_ENTRIES];
 };
+
+static struct history *historys;
+static inline struct history *init_historys() {
+    return calloc(sizeof(struct history), 1);
+}
+
+static inline void destroy_historys() {
+    free(historys);
+}
+
+static inline void clear_entries_in_history() {
+    int i;
+    for (i = 0; i < historys->entry_count; i++) {
+        free(historys->entries[i]);
+    }
+    historys->cur = historys->entry_count = 0;
+}
+
+
+extern int add_entry_to_history(const char * ) __THROW;
+extern int __history(int * , char *[]);
 
 /** function called when shell is terminated 
  *  free all memory requested in history module
 */
-extern void free_all_in_history();   
+extern void _free_all_in_history();   
 
 #endif
